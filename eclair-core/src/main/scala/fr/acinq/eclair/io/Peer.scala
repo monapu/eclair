c249527bd466944c35e20bd5af1c65b079ca8e71
monapu <mona@mona>
2017-08-13 12:46:14 +0900

    Transportの切り変え機能を無効にする、同じノードに複数のチャンネルを開くときの動作が不安定

--- eclair-core/src/main/scala/fr/acinq/eclair/io/Peer.scala
+++ eclair-core/src/main/scala/fr/acinq/eclair/io/Peer.scala
@@ -218,6 +218,7 @@ class Peer(nodeParams: NodeParams, remoteNodeId: PublicKey, address_opt: Option[
       }
       stay using d.copy(channels = channels -- channelIds)
 
+  /*
     case Event(h: HandshakeCompleted, ConnectedData(oldTransport, _, channels)) =>
       log.info(s"got new transport while already connected, switching to new transport")
       context unwatch oldTransport
@@ -226,6 +227,7 @@ class Peer(nodeParams: NodeParams, remoteNodeId: PublicKey, address_opt: Option[
       val c: Set[OfflineChannel] = channels.map(c => HotChannel(c._1, c._2)).toSet
       self ! h
       goto(DISCONNECTED) using DisconnectedData(c)
+  */
   }
 
   onTransition {
